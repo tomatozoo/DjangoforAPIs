63068be70c7f0c678328ee29ab3b7af3b9788c2c
박용주 <enkeejuniour@snu.ac.kr>
2022-10-01 17:17:36 +0900

    ch6) 완료

--- ch5_blog_API/posts/views.py
+++ ch5_blog_API/posts/views.py
@@ -1,5 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+
+
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 # Create your views here.
@@ -11,5 +14,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
